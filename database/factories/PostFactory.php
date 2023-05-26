dcc757387763d2a3a64bebc14c31e0ce6c070b21
myphic <cuworow@gmail.com>
2023-05-26 20:47:56 +0300

    Правки отступов

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -11,21 +11,22 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class PostFactory extends Factory
 {
-    protected $model = Post::class;
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition()
-    {
-        return [
-            'user_id' => User::factory(),
-            'title' => $this->faker->realText(rand(25, 30)),
-            'image' => $this->faker->image(storage_path('app/public/images'),400,300, null, false),
-            'body' => $this->faker->realText(rand(200, 300)),
-            'created_at' => $this->faker->dateTimeBetween('-60 days', '-30 days'),
-            'updated_at' => $this->faker->dateTimeBetween('-20 days', '-1 days'),
-        ];
-    }
+	protected $model = Post::class;
+
+	/**
+	 * Define the model's default state.
+	 *
+	 * @return array<string, mixed>
+	 */
+	public function definition()
+	{
+		return [
+			'user_id' => User::factory(),
+			'title' => $this->faker->realText(rand(25, 30)),
+			'image' => $this->faker->image(storage_path('app/public/images'), 400, 300, null, false),
+			'body' => $this->faker->realText(rand(200, 300)),
+			'created_at' => $this->faker->dateTimeBetween('-60 days', '-30 days'),
+			'updated_at' => $this->faker->dateTimeBetween('-20 days', '-1 days'),
+		];
+	}
 }
