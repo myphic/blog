3d822ac70c2c207f32668d676f90f47c08aa84c1
myphic <cuworow@mail.ru>
2023-06-04 21:12:11 +0300

    Исправлена ошибка с тайтлом, добавил новую верстку формы входа

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,15 +5,19 @@
 	<form method="post" action="{{ route('auth.auth') }}">
 		@csrf
 		<div class="form-group">
+			<label for="email">Email:</label>
 			<input type="email" class="form-control" name="email" placeholder="Адрес почты"
-				   required maxlength="255" value="{{ old('email') ?? '' }}">
+				   required maxlength="255" value="{{ old('email') ?? '' }}" aria-describedby="emailHelp" id="email">
+			<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
 		</div>
 		<div class="form-group">
-			<input type="text" class="form-control" name="password" placeholder="Ваш пароль"
-				   required maxlength="255" value="">
+			<label for="pass">Пароль:</label>
+			<input type="password" class="form-control" id="pass" placeholder="Пароль" required>
 		</div>
-		<div class="form-group">
-			<button type="submit" class="btn btn-info text-white">Войти</button>
+		<div class="form-check">
+			<input type="checkbox" class="form-check-input" id="check">
+			<label class="form-check-label" for="check">Запомнить меня</label>
 		</div>
+		<button type="submit" class="btn btn-primary">Войти</button>
 	</form>
 @endsection
