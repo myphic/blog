2e23ce4e31063c1435baeeacfe11e46b508a12de
myphic <cuworow@mail.ru>
2023-06-28 07:48:23 +0300

    Правки отображения вкладок в админке

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
 	public function index()
 	{
-		return view('admin.index', ['postCount' => Post::count()]);
+		return view('admin.index', ['postCount' => Post::count(), 'userCount' => User::count()]);
 	}
 }
